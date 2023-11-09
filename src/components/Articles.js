d8569da459a082fc4bad9437d69c838682a35164
ethanab <109964884+ethanab@users.noreply.github.com>
2023-11-09 12:44:46 +0000

    wip

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,16 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
-
+import { Link, useSearchParams} from "react-router-dom"
 // Import Link and useSearchParams from React Router
 
 export default function Articles () {
   const articles = useSelector(selectArticles);
 
   // Grab URLSearchParams object from useSearchParams hook
+
+  const [searchParams, setSearchParams] = useSearchParams('')
   
   // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const title = searchParams.get('title');
 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
@@ -21,10 +23,10 @@ export default function Articles () {
       <ul>
         { filteredArticles.map(article => (
           <li key={article.slug}>
-            {/* Replace these a tags! */}
-            <a href={`${article.slug}`}>
+            
+            <Link to={`${article.slug}`} >
               {article.title}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
